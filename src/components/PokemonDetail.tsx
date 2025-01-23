44b6d5fe45c04d8c5d3649012feb5b41c21b54e3
Stephen <stephenn649@gmail.com>
2025-01-23 16:50:24 -0600

    Got the stats table to show, working on moves, and then version images

--- src/components/PokemonDetail.tsx
+++ src/components/PokemonDetail.tsx
@@ -1,13 +1,30 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { formatName } from "../helpers";
 import { PokeDetailTypes } from "../types";
 
+interface bgKeys {
+  grass: {
+    bg: string;
+    color: string;
+  };
+}
+const bg: bgKeys = {
+  grass: {
+    bg: "bg-emerald-400",
+    color: "text-black",
+  },
+};
+
 const PokemonDetail = <T,>() => {
   const { name } = useParams();
-  const [pokemonDetails, setPokemonDetails] = useState<PokeDetailTypes<T> | null>(null);
+  const [pokemonDetails, setPokemonDetails] =
+    useState<PokeDetailTypes<T> | null>(null);
   const [showDetails, setShowDetails] = useState<boolean>(false);
+  // const [type, setType] = useState<string>("");
+
+  const containerRef = useRef(null);
 
   useEffect(() => {
     const getDetails = async () => {
@@ -26,24 +43,74 @@ const PokemonDetail = <T,>() => {
   }, [name]);
 
   useEffect(() => {
-    console.log(pokemonDetails);
-    setShowDetails(true);
+    if (pokemonDetails) {
+      // setType(pokemonDetails.types[0].type.name);
+      setShowDetails(true);
+    }
   }, [pokemonDetails]);
 
   return (
     <div>
       {showDetails && pokemonDetails ? (
         <div className="w-full h-full">
-          <div className="text-center font-bold text-xl">{formatName(name as string)}</div>
+          <div className="text-center font-bold text-xl">
+            {formatName(name as string)}
+          </div>
           <div className="flex justify-center">
-            <img src={pokemonDetails.sprites.front_default} alt={name} />
+            <div className="flex flex-col mb-5">
+              <img
+                src={pokemonDetails.sprites.front_default}
+                alt={name}
+                width="200"
+                height="200"
+              />
+              <button className="border rounded p-2 bg-green-800 hover:bg-green-400 hover:text-black animate-all duration-500">
+                Add to your team
+              </button>
+            </div>
           </div>
 
           {/* container */}
-          <div className="w-full h-full border rounded-lg">
+          <div className="text-center text-lg pb-2">Pokedex Entry</div>
+          <div
+            ref={containerRef}
+            className={`w-full h-full border rounded-lg shadow-lg bg-slate-300 text-black flex py-5`}
+          >
+            <div className="text-center px-5 mx-5">
+              <p>Info/Stats</p>
+              <div>
+                <table>
+                  <thead>
+                    <tr className="border border-black">
+                      <th className="w-32">Name</th>
+                      <th className="w-32">Stat</th>
+                    </tr>
+                  </thead>
+                  <tbody className="rounded-b-xl">
+                    {pokemonDetails.stats.map((stat, i) => (
+                      <tr key={`tr-stat-${i}`} className={`border border-black py-2`}>
+                        <td>{formatName(stat.stat.name)}</td>
+                        <td>{stat.base_stat}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
 
+            <div className="text-center mx-5 px-5">
+              <p>Moves</p>
+              <select>
+                {/* <option></option> */}
+                {pokemonDetails.moves.map((move, i) => (
+                  <option key={`move-${i}`} className="even:bg-slate-300">
+                    {move.move.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="text-center px-5 mx-5">Versions</div>
           </div>
-
         </div>
       ) : null}
     </div>
