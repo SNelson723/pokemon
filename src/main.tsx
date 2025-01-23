2d1c4ed15f80beff61d5a3e21648053757acc07f
Stephen <stephenn649@gmail.com>
2025-01-23 11:02:10 -0600

    Added initial setup of BrowserRouter from react-router in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,21 @@ import App from "./App.tsx";
 
 import { Provider } from "react-redux";
 import { store } from "./store/index.ts";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* store */}
     <Provider store={store}>
+      {/* router */}
+      <BrowserRouter>
+      {/* App */}
+        <Routes>
+          <Route path='/' element={<App />}>
+            <Route path='team' element={<div>My Team</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <App />
     </Provider>
   </StrictMode>
